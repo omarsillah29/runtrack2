3b413f5a8b7133a9b2472380a0d6ff545a2d54d7
omarsillah29 <omar.sillah@laplateforme.io>
2025-07-23 09:21:48 +0200

    jour02

---
+++ jour02/job06/index.php
@@ -0,0 +1,11 @@
+<?php
+$largeur = 20;  // Largeur du rectangle
+$hauteur = 10;  // Hauteur du rectangle
+
+for ($i = 0; $i < $hauteur; $i++) {
+    for ($j = 0; $j < $largeur; $j++) {
+        echo "*";
+    }
+    echo "<br />";
+}
+?>
