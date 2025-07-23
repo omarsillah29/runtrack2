204b37b5d71d8dcaa73efc7d46067e5494a7b86b
omarsillah29 <omar.sillah@laplateforme.io>
2025-07-23 11:41:47 +0200

    jour02

--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -2,7 +2,7 @@
 $largeur = 20;  // Largeur du rectangle
 $hauteur = 10;  // Hauteur du rectangle
 
-for ($i = 0; $i < $hauteur; $i++) {
+for ($i = 0; $i <$hauteur; $i++) {
     for ($j = 0; $j < $largeur; $j++) {
         echo "*";
     }
