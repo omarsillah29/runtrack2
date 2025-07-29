806455d84871f0d9af2e5ace54236f7413ebb890
omarsillah29 <omar.sillah@laplateforme.io>
2025-07-29 14:37:18 +0200

    job07 corrigé

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -1,55 +1,54 @@
-
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Maison</title>
+    <title>Maison ASCII</title>
 </head>
-
 <body>
 
     <form method="post">
         <label>Largeur :</label>
-        <input type="text" name="largeur">
+        <input type="text" name="largeur" required>
 
         <label>Hauteur :</label>
-        <input type="text" name="hauteur">
+        <input type="text" name="hauteur" required>
 
         <input type="submit" value="Envoyer">
     </form>
 
-
     <pre>
 <?php
 if (isset($_POST['largeur']) && isset($_POST['hauteur'])) {
-    $largeur = $_POST['largeur'];
-    $hauteur = $_POST['hauteur'];
+    $largeur = intval($_POST['largeur']);
+    $hauteur = intval($_POST['hauteur']);
 
     if ($largeur > 2 && $hauteur > 2) {
+        $demiLargeur = intval($largeur / 2);
+
         // Dessin du toit
-        for ($i = 0; $i < $largeur / 2; $i++) {
-            $espaces = ($largeur / 2) - $i;
+        for ($i = 0; $i < $demiLargeur; $i++) {
+            $espaces = $demiLargeur - $i;
             echo str_repeat(" ", $espaces);
             echo "/";
             echo str_repeat("-", $i * 2);
             echo "\\";
             echo "\n";
         }
-        // les murs
+
+        // Dessin des murs
         for ($i = 0; $i < $hauteur; $i++) {
             echo " " . "|" . str_repeat(" ", $largeur - 2) . "|\n";
         }
-        // la base
+
+        // Dessin de la base
         echo " " . str_repeat("-", $largeur) . "\n";
+    } else {
+        echo "Les dimensions doivent être supérieures à 2.\n";
     }
 }
 ?>
-</pre>
-
-
+    </pre>
 
 </body>
-
 </html>
